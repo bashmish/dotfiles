a23861b0baea886f2fb4a6f5fa9d7963845f61de
Mikhail Bashkirov <bashmish@gmail.com>
2020-01-10 23:22:58 +0100

    [shell] Automated installation of oh-my-zsh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 cd ~
+git clone https://github.com/ohmyzsh/ohmyzsh.git .oh-my-zsh
 ln -s ./.dotfiles/.fixpackrc .fixpackrc
 ln -s ./.dotfiles/.tmux.conf .tmux.conf
 ln -s ./.dotfiles/.zshrc .zshrc
+source .zshrc
