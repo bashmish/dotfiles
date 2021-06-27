2e15c2a5297625e949a3032451aba5150ac33b87
Mikhail Bashkirov <bashmish@gmail.com>
2021-06-27 12:34:59 +0200

    [shell] Automated installation of Homebrew

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 cd ~
 xcode-select --install
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 git clone https://github.com/ohmyzsh/ohmyzsh.git .oh-my-zsh
 ln -s ./.dotfiles/.fixpackrc .fixpackrc
 ln -s ./.dotfiles/.tmux.conf .tmux.conf
